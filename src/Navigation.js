a37bf8380ffb4d72dce0a1e4bd09bce921303d7d
Tanya Shylock <tourtanya@gmail.com>
2023-06-20 22:25:15 -0600

    more updates to styling and hamburger menu behavior

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import userContext from "./userContext";
@@ -6,42 +6,82 @@ import "./Navigation.css";
 
 function Navigation({ logout }) {
   const { currUser } = useContext(userContext);
+  const [expanded, setExpanded] = useState(false);
+
+  const handleLinkClick = () => {
+    if (expanded) {
+      setExpanded(false);
+    }
+  };
 
   return (
-    <Navbar collapseOnSelect expand="md" className="navbar">
+    <Navbar
+      collapseOnSelect
+      expand="md"
+      className="navbar"
+      expanded={expanded}
+    >
       <Navbar.Brand>
         <NavLink to="/" className="nav-link">
           <h2>Yard Hoppers</h2>
         </NavLink>
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse
-        id="responsive-navbar-nav"
-        className="justify-content-end"
-      >
+      <Navbar.Toggle
+        aria-controls="responsive-navbar-nav"
+        onClick={() => setExpanded(!expanded)}
+      />
+      <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
         <Nav>
-          <Nav.Link as={NavLink} to="/listings">
+          <Nav.Link
+            as={NavLink}
+            to="/listings"
+            onClick={handleLinkClick}
+          >
             Listings
           </Nav.Link>
           {!currUser && (
             <>
-              <Nav.Link as={NavLink} to="/login">
+              <Nav.Link
+                as={NavLink}
+                to="/login"
+                onClick={handleLinkClick}
+              >
                 Login
               </Nav.Link>
-              <Nav.Link as={NavLink} to="/signup">
+              <Nav.Link
+                as={NavLink}
+                to="/signup"
+                onClick={handleLinkClick}
+              >
                 Sign Up
               </Nav.Link>
             </>
           )}
           {currUser && (
             <>
-              <Nav.Link as={NavLink} to="/new-listing">
+              <Nav.Link
+                as={NavLink}
+                to="/new-listing"
+                onClick={handleLinkClick}
+              >
                 Create a Listing
               </Nav.Link>
-              <Nav.Link as={NavLink} to="/profile">
+              <Nav.Link
+                as={NavLink}
+                to="/profile"
+                onClick={handleLinkClick}
+              >
                 Profile
               </Nav.Link>
-              <Nav.Link as={NavLink} to="/" onClick={logout} className="logout">
+              <Nav.Link
+                as={NavLink}
+                to="/"
+                onClick={() => {
+                  handleLinkClick();
+                  logout();
+                }}
+                className="logout"
+              >
                 Log out {currUser.username}
               </Nav.Link>
             </>
