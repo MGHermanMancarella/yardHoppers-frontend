69ee2c443cb2798f258a5d44b0f66e130744545b
Michael <mherman315@gmail.com>
2023-07-26 17:27:29 -0700

    buttons on home:
    added login + signup buttons on homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import userContext from "./userContext";
 import "./Homepage.css";
 import logo from "./logo.png";
+import { Link } from "react-router-dom";
 
 /** Homepage for Yard Hoppers
  *
@@ -15,20 +16,22 @@ import logo from "./logo.png";
 function Homepage() {
   const { currUser } = useContext(userContext);
 
-
   return (
-    <div className="home">
-
-
-      <header className="home-header">
-        <img src={logo} className="home-logo" alt="logo" />
-        <div className="title">
-        {currUser && <h3>Welcome, {currUser.username}!</h3>}
+    <div className='home'>
+      <header className='home-header'>
+        <img src={logo} className='home-logo' alt='logo' />
+        <div className='title'>
+          {currUser && <h3>Welcome, {currUser.username}!</h3>}
           <h1>Yard Hoppers</h1>
           <h4>Get on my lawn!</h4>
         </div>
       </header>
-
+      <Link to='/login'>
+        <button class='btn btn-success mx-4'>Login</button>
+      </Link>
+      <Link to='/signup'>
+        <button class='btn btn-success'>Sign Up</button>
+      </Link>
     </div>
   );
 }
